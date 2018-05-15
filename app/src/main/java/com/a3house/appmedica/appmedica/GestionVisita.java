9d4305ec28e4f3ede2ecfbdb20bf6f4398c8ab4f
Daniel <daniel.zaplana.sanchez@gmail.com>
2018-05-15 19:12:47 +0200

    GestionVisita: Modificar métodos

--- app/src/main/java/com/a3house/appmedica/appmedica/GestionVisita.java
+++ app/src/main/java/com/a3house/appmedica/appmedica/GestionVisita.java
@@ -11,20 +11,24 @@ public class GestionVisita {
      * Default constructor
      */
     public GestionVisita() {
+
     }
 
     /**
      * 
      */
-    public void crearVisita() {
-        // TODO implement here
+    public void crearVisita(Calendar fecha, String lugar, String doctor, String notas) {
+        // TODO Sólo crea un objeto Visita... quizás innecesaria esta función
+        Visita visita = new Visita(fecha, lugar, doctor, notas);
     }
 
     /**
      * 
      */
     public void mostrarVisitas() {
-        // TODO implement here
+        // TODO Pedirá los datos en GestionFirebase
+        // Get de la información desde GestionFirebase
+
     }
 
 }
