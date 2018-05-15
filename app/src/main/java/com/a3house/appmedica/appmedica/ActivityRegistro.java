ed819fd0494b622cc6e0d6cb3353e6adb737b907
Miguel <mgarcia711@gmail.com>
2018-05-15 17:36:36 +0200

    Diseño XML activity_registro actualizado

---
+++ app/src/main/java/com/a3house/appmedica/appmedica/ActivityRegistro.java
@@ -0,0 +1,13 @@
+package com.a3house.appmedica.appmedica;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityRegistro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro);
+    }
+}
