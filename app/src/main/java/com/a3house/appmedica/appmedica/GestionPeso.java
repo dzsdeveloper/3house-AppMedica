2d8cc087b8b9ff245c80a3b049b144efcc72e424
victor <victor@telefonica.es>
2018-05-16 18:37:04 +0200

    GestionPeso - Creado metodo calcularIMC(Usuario usuario, Peso peso) - Creado metodo clasificacionIMC(Peso peso) - Creado metodo calcularVariacion(Peso pesoAnterior, Peso pesoNuevo) - Creado metodo  calcularPesoIdeal(Usuario usuario)

--- app/src/main/java/com/a3house/appmedica/appmedica/GestionPeso.java
+++ app/src/main/java/com/a3house/appmedica/appmedica/GestionPeso.java
@@ -2,6 +2,10 @@ package com.a3house.appmedica.appmedica;
 
 import java.util.*;
 
+
+import com.a3house.appmedica.appmedica.Usuario.*;
+
+
 /**
  * 
  */
@@ -17,35 +21,101 @@ public class GestionPeso {
      * 
      */
     public void introducirPeso() {
-        // TODO implement here
+        // TODO Hablar de este tema con el equipo para ver que vamos a hacer
+
     }
 
     /**
-     * 
+     * val= peso
+       alt= altura
+       imc = el índice de masa corporal
+       la formula para calcular el imc es imc= val/(alt*alt)
      */
-    public void calcularIMC() {
-        // TODO implement here
+    public double calcularIMC(Usuario usuario, Peso peso) {
+        // TODO  recibir los datos del Firebase sobre el usuario, comprobar que la division de double entre int no da problemas
+        //Clase Usuario altura en centimetros
+        //Clase Peso valor en Kilos
+        double imc;
+
+        //La formula es  imc= val/(alt*alt)
+        imc = peso.getValor()/(usuario.getAltura()*usuario.getAltura());
+
+
+
+            return  imc;
     }
 
     /**
-     * 
+     * La funcion recibe el objeto Peso de la base de datos y nos devuelve un String
+     * Indicando cual es la clasificación de nuestro peso
+     * imc = el índice de masa corporal
+     * cad = mensaje que nos indica nuestra clasificacion de peso
      */
-    public void calcularVariacion() {
-        // TODO implement here
+    public String clasificacionIMC(Peso peso) {
+
+        String cad="";
+        if(peso.getImc()<16.00){
+            cad="Infrapeso: Delgadez Severa";
+        }else if(peso.getImc()<=16.00 || peso.getImc()<=16.99){
+            cad="Infrapeso: Delgadez moderada";
+        }else if(peso.getImc()<=17.00 ||peso.getImc()<=18.49){
+            cad="Infrapeso: Delgadez aceptable";
+        }else if(peso.getImc()<=18.50 || peso.getImc()<=24.99){
+            cad="Peso Normal";
+        }else if(peso.getImc()<=25.00 || peso.getImc()<=29.99){
+            cad="Sobrepeso: grado I";
+        }else if(peso.getImc()<=30.00 || peso.getImc()<=34.99){
+            cad="Sobrepeso: grado II";
+        }else if(peso.getImc()<=35.00 || peso.getImc() <=40.00){
+            cad="Sobrepeso: grado III";
+        }else{
+            cad="no existe clasificacion";
+        }
+
+        return cad;
+
+    }
+
+    /**
+     * La funcion recibe el ultimo peso registrado pesoAnterior y el nuevo peso que hemos introducido pesoNuevo
+     * Hace la resta del pesoNuevo menos el pesoAnterior, de tal forma que el resultado
+     * si es positivo +cantidad sera el peso que hemos engordado
+     * si es negativo -cantidad sera el peso que hemos adelgazado
+     */
+    public double calcularVariacion(Peso pesoAnterior, Peso pesoNuevo) {
+        // TODO revisar si hay que inicializarlo
+
+        //double pesoFinal = 0;
+
+        double pesoFinal = pesoNuevo.getValor()-pesoAnterior.getValor();
+
+        return pesoFinal;
     }
 
     /**
      * 
      */
     public void mostrarPesos() {
-        // TODO implement here
+        // TODO hay que vincular los textView mediante data Binding
     }
 
     /**
-     * 
+     * La funcion recibe la altura registrada en Usuario y el valor registrado en Peso
+     * Realiza el calculo mediante la formula de Lorentz
      */
-    public void calcularPesoIdeal() {
+    public double calcularPesoIdeal(Usuario usuario) {
         // TODO implement here
+        //La fórmula de Lorentz
+        //Peso ideal masculino = estatura (en cm) – 100 – [(estatura en cm – 150) / 4]
+        //Peso ideal femenino = estatura (en cm) – 100 – [(estatura en cm – 150) / 2,5]
+
+        double idealMasculino = usuario.getAltura()-100 - ((usuario.getAltura() -150)/4);
+        double idealFemenino = usuario.getAltura() - 100 -((usuario.getAltura() -150)/4);
+
+        //Si el usuario es un hombre en el atributo sexo estara marcado como 'H'
+        if(usuario.getSexo()=='H'){return idealMasculino;}
+        else{return idealFemenino;}
+
     }
 
 }
