a5e8bfce0ed0d30bab3d909a04e77b5b440b391d
Daniel <daniel.zaplana.sanchez@gmail.com>
2018-05-16 18:22:16 +0200

    GestionVisita.class -> Modificar métodos

--- app/src/main/java/com/a3house/appmedica/appmedica/GestionVisita.java
+++ app/src/main/java/com/a3house/appmedica/appmedica/GestionVisita.java
@@ -1,6 +1,9 @@
 package com.a3house.appmedica.appmedica;
 
+import android.support.v7.widget.RecyclerView;
+
 import java.util.*;
+import com.a3house.appmedica.appmedica.GestionFirebase.class;
 
 /**
  * 
@@ -15,19 +18,22 @@ public class GestionVisita {
     }
 
     /**
-     * 
+     * Crea objeto visita y lo envía a través del método enviarDatosVisita de la clase GestionFirebase
      */
     public void crearVisita(Calendar fecha, String lugar, String doctor, String notas) {
-        // TODO Sólo crea un objeto Visita... quizás innecesaria esta función
+        // TODO Falta añadir en GestionFirebase.enviarDatosVisita la variable que recibirá (visita)
         Visita visita = new Visita(fecha, lugar, doctor, notas);
+        GestionFirebase gf = new GestionFirebase();
+        gf.enviarDatosVisita(visita);
     }
 
     /**
      * 
      */
     public void mostrarVisitas() {
-        // TODO Pedirá los datos en GestionFirebase
-        // Get de la información desde GestionFirebase
+        // TODO Pendiente. Falta saber como recibirá los datos de GestionFirebase
+        GestionFirebase gf = new GestionFirebase();
+        gf.recibirDatos();
 
     }
 
