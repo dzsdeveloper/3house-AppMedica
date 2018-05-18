84aed4edaf749ca7bd209c15dd55108ec647e928
Miguel <mgarcia711@gmail.com>
2018-05-18 18:41:43 +0200

    res/strings

--- app/src/main/java/com/a3house/appmedica/appmedica/ActivityNuevaVisita.java
+++ app/src/main/java/com/a3house/appmedica/appmedica/ActivityNuevaVisita.java
@@ -1,7 +1,11 @@
 package com.a3house.appmedica.appmedica;
 
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class ActivityNuevaVisita extends AppCompatActivity {
 
@@ -9,5 +13,8 @@ public class ActivityNuevaVisita extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nueva_visita);
+        //Soporta la toolbar para mostrar el menu
+        //Toolbar toolbarNuevaVisita = (Toolbar) findViewById(R.id.toolbarNuevaVisita);
+        //setSupportActionBar(toolbarNuevaVisita);
     }
 }
